6f339298034fd040e976f3e6c6ee732db2f6c95b
Ben Wadsworth <benjaminwadsworth@gmail.com>
2025-04-11 10:53:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# minui-psp
+
+A [MinUI](https://github.com/shauninman/MinUI) and [NextUI](https://github.com/LoveRetro/NextUI) Emu Pak for PSP, wrapping the standalone PPSSPP emulator.
+
+## Requirements
+
+This pak is designed and tested on the following MinUI Platforms and devices:
+
+- `tg5040`: Trimui Brick (formerly `tg3040`)
+
+The pak may work on other platforms and devices, but it has not been tested on them.
+
+## Installation
+
+1. Mount your MinUI SD card.
+2. Download the latest release from Github. It will be named `PSP.pak.zip`.
+3. Copy the zip file to the correct platform folder in the "/Emus" directory on the SD card. Please ensure the new zip file name is `PSP.pak.zip`.
+4. Extract the zip in place, then delete the zip file.
+5. Confirm that there is a `/Emus/$PLATFORM/PSP.pak/launch.sh` file on your SD card.
+6. Create a folder at `/Roms/PlayStation Portable (PSP)` and place your roms in this directory.
+7. Unmount your SD Card and insert it into your MinUI device.
+
+## Saves & States
+
+- Save states are stored in the `/.userdata/shared/PSP-ppsspp/` directory.
+- Game saves are stored in the `/Saves/PSP/` directory.
+
+## Credits
+
+- [hrydgard](https://github.com/hrydgard) for developing [PPSSPP](https://github.com/hrydgard/ppsspp) and related projects
+- [Shaun Inman](https://github.com/shauninman) for developing [MinUI](https://github.com/shauninman/MinUI).
+- [ro8inmorgan](https://github.com/ro8inmorgan), [frysee](https://github.com/frysee) and the rest of the NextUI contributors for developing [NextUI](https://github.com/LoveRetro/NextUI).
+
+## License
+
+This project is based on PPSSPP, which is open-source software. Please refer to the original PPSSPP license for more information.
---
+++ launch.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+set -eo pipefail
+set -x
+
+rm -f "$LOGS_PATH/PSP.txt"
+exec >>"$LOGS_PATH/PSP.txt"
+exec 2>&1
+
+echo $0 $*
+
+mkdir -p "$USERDATA_PATH/PSP-ppsspp"
+EMU_DIR="$SDCARD_PATH/Emus/$PLATFORM/PSP.pak/PPSSPPSDL"
+PACK_DIR="$SDCARD_PATH/Emus/$PLATFORM/PSP.pak"
+
+export PATH="$EMU_DIR:$PACK_DIR/bin:$PATH"
+export LD_LIBRARY_PATH="$EMU_DIR/libs:$PACK_DIR/lib:$LD_LIBRARY_PATH"
+
+cleanup() {
+    rm -f /tmp/stay_awake
+
+    if [ -f "$USERDATA_PATH/PSP-ppsspp/cpu_governor.txt" ]; then
+        cat "$USERDATA_PATH/PSP-ppsspp/cpu_governor.txt" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        rm -f "$USERDATA_PATH/PSP-ppsspp/cpu_governor.txt"
+    fi
+    if [ -f "$USERDATA_PATH/PSP-ppsspp/cpu_min_freq.txt" ]; then
+        cat "$USERDATA_PATH/PSP-ppsspp/cpu_min_freq.txt" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        rm -f "$USERDATA_PATH/PSP-ppsspp/cpu_min_freq.txt"
+    fi
+    if [ -f "$USERDATA_PATH/PSP-ppsspp/cpu_max_freq.txt" ]; then
+        cat "$USERDATA_PATH/PSP-ppsspp/cpu_max_freq.txt" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        rm -f "$USERDATA_PATH/PSP-ppsspp/cpu_max_freq.txt"
+    fi
+
+    umount "$EMU_DIR/.config/ppsspp/PSP/SAVEDATA" || true
+    umount "$EMU_DIR/.config/ppsspp/PSP/PPSSPP_STATE" || true
+}
+
+main() {
+    echo "1" >/tmp/stay_awake
+    trap "cleanup" EXIT INT TERM HUP QUIT
+
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >"$USERDATA_PATH/PSP-ppsspp/cpu_governor.txt"
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq >"$USERDATA_PATH/PSP-ppsspp/cpu_min_freq.txt"
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq >"$USERDATA_PATH/PSP-ppsspp/cpu_max_freq.txt"
+    echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo 1608000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+    echo 2000000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+
+    cd "$EMU_DIR"
+    export HOME="$EMU_DIR"
+
+    mkdir -p "$SDCARD_PATH/Saves/PSP"
+    mkdir -p "$EMU_DIR/.config/ppsspp/PSP/SAVEDATA"
+    mount -o bind "$SDCARD_PATH/Saves/PSP" "$EMU_DIR/.config/ppsspp/PSP/SAVEDATA"
+
+    mkdir -p "$SHARED_USERDATA_PATH/PSP-ppsspp"
+    mkdir -p "$EMU_DIR/.config/ppsspp/PSP/PPSSPP_STATE"
+    mount -o bind "$SHARED_USERDATA_PATH/PSP-ppsspp" "$EMU_DIR/.config/ppsspp/PSP/PPSSPP_STATE"
+
+    "$EMU_DIR/PPSSPPSDL" "$*"
+}
+
+main "$@"
